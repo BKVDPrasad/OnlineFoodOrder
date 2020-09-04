76d90b3ef7747408226de07f16b9f7e659db26e6
BKVDPrasad <BKVDPrasad@users.noreply.github.com>
2020-09-04 15:18:41 +0530

    state is did
    state operations are done

--- OnlineFood/pwn/views.py
+++ OnlineFood/pwn/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
-from pwn.models import AdminLoginModel
+from pwn.models import AdminLoginModel,StateModel
+from django.contrib import messages
 
 def showIndex(request):
     return render(request,"pwn/login.html")
@@ -25,7 +26,8 @@ def welcome(request):
 
 
 def openState(request):
-    return render(request,"pwn/openstate.html")
+    sm = StateModel.objects.all()
+    return render(request,"pwn/openstate.html",{'data':sm})
 
 
 def openCity(request):
@@ -42,3 +44,28 @@ def openVendor(request):
 
 def openReporsts(request):
     return render(request,"pwn/openreports.html")
+
+
+def savestate(request):
+    StateModel(name=request.POST.get('t1'),photo=request.FILES['t2']).save()
+    messages.success(request,'state is saved')
+    return openState(request)
+
+
+def updatestate(request):
+    sid = request.GET.get('id')
+    print(sid)
+    sm = StateModel.objects.get(id=sid)
+
+    return render(request,'pwn/openstate.html',{'udata':sm})
+
+
+def updatestateid(request):
+    StateModel.objects.filter(id=request.GET.get('sid')).update(name=request.POST.get('t1'),photo=request.FILES.get('t2'))
+    return redirect('state')
+
+
+def sdelete(request):
+    StateModel.objects.filter(id=request.GET.get('sid')).delete()
+    messages.success(request,'state deleted')
+    return redirect('state')
